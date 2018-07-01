8df7c00b935f784ca2fccca758fdd24daf575d97
Chris Wendt <chrismwendt@gmail.com>
2018-07-01 14:57:01 -0700

    WIP

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,23 @@
-{-# LANGUAGE OverloadedStrings #-}
+import qualified System.Process as P
+import qualified System.Process.Typed as T
+import qualified Data.ByteString.Lazy as BL
 
-module Main where
+-- Run: stack runghc bug.hs
+-- Notice the process finishes.
+--
+-- Then run: stack ghc bug.hs ; ./bug
+-- The process hangs!
 
-main :: IO ()
 main = do
-  putStr "hello, world\n"
+  putStrLn "System.Process with 10k works:"
+  P.readCreateProcess (P.shell "sh -c \"yes | head -c 10000\"") "" >>= print . length
 
-  return ()
+  putStrLn "System.Process.Typed with 10k also works:"
+  T.readProcessStdout_ (T.shell "sh -c \"yes | head -c 10000\"") >>= print . BL.length
+
+  putStrLn "System.Process with 100k works:"
+  P.readCreateProcess (P.shell "sh -c \"yes | head -c 100000\"") "" >>= print . length
+
+  putStrLn "System.Process.Typed with 100k works in ghci, but hangs (!) if compiled."
+  putStrLn "When you Ctrl-C, you'll see ~65k lines printed (suspiciously close to 65536, a power of 2)."
+  T.readProcessStdout_ (T.shell "sh -c \"yes | head -c 100000\"") >>= print . BL.length
